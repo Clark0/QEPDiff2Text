8daeb00c87c65df4e1f0b802eddfc9d09b26951f
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-11-03 00:14:54 +0800

    Added QEPFetcher

---
+++ src/QEPFetcher.py
@@ -0,0 +1,15 @@
+class QEPFetcher:
+    def __init__(self, host, dbname, user, password):
+        self.conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+        self.cur = self.conn.cursor()
+    
+    def fetch_json(self, query):
+        try:
+            self.cur.execute('explain (format json) ' + query)
+            analyze_fetched = self.cur.fetchall()[0][0][0]['Plan']
+            self.conn.rollback()
+            return analyze_fetched
+        except:
+            pass
+        self.conn.rollback()
+        return None
